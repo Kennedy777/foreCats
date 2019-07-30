4099b7ee0d38aa6fe5e543bc06ddc44e3cbb027e
Kennedy777 <reach.marguerite@gmail.com>
2019-07-30 15:16:00 -0700

    move env and gitignore files

---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'index.css';
+import App from 'App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+const Index = () => {
+  return <div>Weather ForeCats</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
